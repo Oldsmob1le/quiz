87182077a840e52be9852d6f6c17c4b8e1629b8e
Oldsmob1le <abdulin00@vk.com>
2024-05-13 08:54:28 +0300

    third commit

--- welcome.php
+++ welcome.php
@@ -37,7 +37,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-        <a class="navbar-brand" href="#"><b>Online Quiz System</b></a>
+        <a class="navbar-brand" href="#"><b>Online Quiz System 2</b></a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
