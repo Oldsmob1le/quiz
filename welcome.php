c63d950d1c62c4765e3f10edd48357f118f3d178
Oldsmob1le <abdulin00@vk.com>
2024-05-13 08:43:54 +0300

    second commit

--- welcome.php
+++ welcome.php
@@ -19,7 +19,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome | Online Quiz System</title>
+    <title>Welcome | Online Quiz System 1</title>
     <link  rel="stylesheet" href="css/bootstrap.min.css"/>
     <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>    
     <link rel="stylesheet" href="css/welcome.css">
